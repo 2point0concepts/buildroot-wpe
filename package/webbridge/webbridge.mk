5d50a260c20b26dedef116ddf5b75f0e56cfdbaa
haseenamol@tataelxsi.co.in <haseenamol@tataelxsi.co.in>
2016-07-15 11:41:46 +0000

    WEBDRIVER changes

--- package/webbridge/webbridge.mk
+++ package/webbridge/webbridge.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WEBBRIDGE_VERSION = 8032586b6516b465a26acf7ad13d52057a2ecb5a
+WEBBRIDGE_VERSION = a7d8212e82477ae0048da2f9ee1503c0a9b39fe5 
 WEBBRIDGE_SITE_METHOD = git
 WEBBRIDGE_SITE = git@github.com:Metrological/webbridge.git
 WEBBRIDGE_INSTALL_STAGING = YES
